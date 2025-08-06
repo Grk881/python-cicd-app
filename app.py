511c3b5f24842f3c81bbf21818d14a0395bffbc0
Ubuntu <ubuntu@ip-172-31-37-177.ec2.internal>
2025-08-06 14:45:45 +0000

    Trigger CI/CD pipeline

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Python CI/CD v2 Pipeline on AWS!"
+    return "Hello from Python CI/CD v3 Pipeline on AWS!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
